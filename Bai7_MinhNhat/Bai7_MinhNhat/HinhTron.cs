25a6acd961cc321f78c036ea705e5d4dcd12eb8f
Luu Thuy Minh Nhat <minhnhat240820032019@gmail.com>
2023-09-06 14:55:09 +0700

    add Bai7_MinhNhat and edit Demo

---
+++ Bai7_MinhNhat/Bai7_MinhNhat/HinhTron.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai7_MinhNhat
+{
+    class HinhTron
+    {
+        double bankinh;
+        ToaDo tam;
+
+        public double Bankinh { get => bankinh; set => bankinh = value; }
+        internal ToaDo Tam { get => tam; set => tam = value; }
+
+        public HinhTron()
+        {
+
+        }
+        public HinhTron(ToaDo tam, double bankinh)
+        {
+            this.tam = tam;
+            this.bankinh = bankinh;
+        }
+        public void xuat()
+        {
+            Console.WriteLine("Ban kinh hinh tron: {0}", bankinh);
+            Console.WriteLine("Chu vi hinh tron la: {0} ", TinhChuVi());
+            Console.WriteLine("Dien tich hinh tron la: {0}", TinhDienTich());
+        }
+        public double TinhChuVi()
+        {
+            return 2 * bankinh * Math.PI;
+        }
+        public double TinhDienTich()
+        {
+            return bankinh * bankinh * Math.PI;
+        }
+    }
+}
---
+++ Bai7_MinhNhat/Bai7_MinhNhat/ToaDo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai7_MinhNhat
+{
+    class ToaDo
+    {
+        string tentd;
+        float x;
+        float y;
+
+        public string Tentd { get => tentd; set => tentd = value; }
+        public float X { get => x; set => x = value; }
+        public float Y { get => y; set => y = value; }
+        public ToaDo() : this("O", 0, 0)
+        {
+        }
+        public ToaDo(string tentd, float x, float y)
+        {
+            this.tentd = tentd;
+            this.x = x;
+            this.y = y;
+        }
+        public void Nhap()
+        {
+            Console.Write("Nhap ten toa do: ");
+            tentd = Console.ReadLine();
+            Console.Write("Nhap x: ");
+            x = float.Parse(Console.ReadLine());
+            Console.Write("Nhap y: ");
+            y = float.Parse(Console.ReadLine());
+        }
+        public void Xuat()
+        {
+            Console.WriteLine("{0}({1}:{2})", tentd, x, y);
+        }
+    }
+}
